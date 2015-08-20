c5fad28898e9a0889eb5a328883a211e5cb9c1e1
Mehreen Tahir <mehreen.tahir@square63.com>
2015-08-20 17:03:29 +0500

    User: Devise Views generated and Username Feild Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
+  validates :username, length: { minimum: 5 }
   # Include default devise modules. Others available are:
   #:lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   # attr_accessible :title, :body
 end
