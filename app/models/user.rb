22cb8cf33f39b83a9e981d1440308d1e8fc49858
Mehreen Tahir <mehreen.tahir@square63.com>
2015-08-20 20:45:30 +0500

    User: User Image Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :products
+  has_one :image, as: :imageable, dependent: :destroy
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :image_attributes
+  accepts_nested_attributes_for :image, allow_destroy: true
+  # attr_accessible :title, :body
+  def picture_url (*params)
+    params.present? ? image.photo.url(params[0]) : image.photo.url
+  end
   # attr_accessible :title, :body
 end
