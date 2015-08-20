05ea81fdff4db895ae6dd869c9b07c56c1afdaff
Mehreen Tahir <mehreen.tahir@square63.com>
2015-08-21 01:05:57 +0500

    Routes: Root Resset to the Product's DashBoard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Eseller::Application.routes.draw do
 
+  root :to => 'products#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -62,7 +63,6 @@ Eseller::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'products#index'
 
   # See how all your routes lay out with "rake routes"
 
