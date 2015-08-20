333eb8ad720d6105378304ff64a65a890ba7be2f
Mehreen Tahir <mehreen.tahir@square63.com>
2015-08-20 14:35:11 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eseller::Application.config.secret_token = 'bfd5e2ea492dc132ecaf3c9f0461f8b3d30ddb48fde63b1b1d2e978602f2d37d39e430c92b32931f5adacf6f3365248c47ceed33417bfe5c5e7ed1848e0dea0b'
