5ff793b1841e60cbfcbd65aae6223147db4bebc3
Mehreen Tahir <mehreen.tahir@square63.com>
2015-08-20 17:35:11 +0500

    User: Configuration Of Authentication Functioanlities

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,6 @@ Eseller::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 end
