d7f6cf1d4c2956caf1ab48b6aa54248c84f698f7
Mehreen Tahir <mehreen.tahir@square63.com>
2015-08-26 12:31:05 +0500

    User: Removed comments for model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
   validates :username, length: { minimum: 5 }
-  # Include default devise modules. Others available are:
-  #:lockable, :timeoutable and :omniauthable
 
   PER_PAGE = 10
 
@@ -11,12 +9,12 @@ class User < ActiveRecord::Base
   has_many :products
   has_many :reviews
   has_one :image, as: :imageable, dependent: :destroy
-  # Setup accessible (or protected) attributes for your model
+
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :image_attributes
   accepts_nested_attributes_for :image, allow_destroy: true
-  # attr_accessible :title, :body
+
   def picture_url (*params)
     params.present? ? image.photo.url(params[0]) : image.photo.url
   end
-  # attr_accessible :title, :body
+
 end
