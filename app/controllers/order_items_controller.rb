d9b54bb4a957e7386fbeaf99f32fadca6ab59a42
Mehreen Tahir <mehreen.tahir@square63.com>
2015-08-31 02:01:02 +0500

    OrderItems: Deletion done with AJAX

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -11,7 +11,6 @@ class OrderItemsController < ApplicationController
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
-    redirect_to :back
   end
 
 end
