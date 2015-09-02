5879aa4cdd2bce84172ac92ad9f2044b07789ad0
Mehreen Tahir <mehreen.tahir@square63.com>
2015-09-02 10:41:46 +0500

    Discount: Pirce helper made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_order
   helper_method :valid_user?
   helper_method :discounted_price
+  helper_method :cart_total_price
 
   def current_order
     if session[:order_id].present? & Order.exists?(session[:order_id])
@@ -20,4 +21,7 @@ class ApplicationController < ActionController::Base
     current_order.orignal_price * (1-(discount_percentage/100.to_f))
   end
 
+  def cart_total_price
+     current_order.is_discounted ? current_order.discounted_price : current_order.orignal_price
+  end
 end
