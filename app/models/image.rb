9dff727c723c3d2618ddfdda16f28efd5322cfc7
Mehreen Tahir <mehreen.tahir@square63.com>
2015-09-03 17:41:58 +0500

    Image: Resizing issue fixed

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,8 +4,8 @@ class Image < ActiveRecord::Base
 
   has_attached_file :photo, 
                     styles: { 
-                      medium: "300x300>",
-                      thumb: "100x100>"
+                      medium: "300x300!",
+                      thumb: "100x100!"
                     },
                     default_url: "/assets/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
