baccfc944cc847c438badbb745af4054c552689c
Mehreen Tahir <mehreen.tahir@square63.com>
2015-09-07 11:44:41 +0500

    Product: Ordered scope removed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   respond_to :html
 
   def index
-    @products = Product.ordered.search_for_index(params)
+    @products = Product.search_for_index(params)
     @order_item = current_order.order_items.new
     respond_with(@products)
   end
