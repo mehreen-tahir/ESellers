1cee3f90c0f56be46e14281195169b1595704779
Mehreen Tahir <mehreen.tahir@square63.com>
2015-09-07 13:23:14 +0500

    OrderItem: UI messages updated

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,7 +1,8 @@
 class OrderItemsController < ApplicationController
 
   def create
-    return false if current_order.order_items.exists?(params[:order_item])
+    @error = is_valid_order_item
+    return false if @error.present?
     @order = current_order
     @order_item = @order.order_items.new(params[:order_item])
     @order_item.save
@@ -21,6 +22,16 @@ class OrderItemsController < ApplicationController
   def update_cart_price
      order_price = current_order.order_items.collect(&:product).collect(&:price).sum
      current_order.update_attribute(:orignal_price, order_price)
+  end
+
+  def is_valid_order_item
+    product = params[:order_item]
+    product_item = Product.find(product["product_id"])
+    if current_order.order_items.exists?(product)
+      return "Item is already in cart!"
+    elsif Product.exists?(product["product_id"]) and valid_user? product_item.user
+      return "You can not add your own item to cart!"
+    end
   end
 
 end
