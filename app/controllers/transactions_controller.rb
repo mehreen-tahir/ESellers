46c3fd15b0d2f460159be39609e03c7f80366a00
Mehreen Tahir <mehreen.tahir@square63.com>
2015-09-08 12:28:43 +0500

    UserMailer: Added user_id for orders and updated views

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -11,8 +11,8 @@ class TransactionsController < ApplicationController
               amount: cart_total_price,
               payment_method_nonce: params[:payment_method_nonce])
     if @result.success?
-      current_order.update_attribute(:status, "completed")
-      UserMailer.order_transaction(current_user, current_order).deliver
+      current_order.update_attributes(status: "completed", user_id: current_user.id)
+      UserMailer.order_transaction(current_user, current_order, cart_total_price).deliver
       session.delete(:order_id)
       redirect_to root_url, notice: "Congraulations! Your transaction has been successfull!"
     else
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,10 @@ class UserMailer < ActionMailer::Base
 
   default from: "paymentsupport@eseller.com"
 
-  def order_transaction(user, order)
+  def order_transaction(user, order, cart_total)
     @user = user
     @order = order
+    @cart_total_price = cart_total
 
     mail to:  @user.email, subject: "Payment recipet."
   end
