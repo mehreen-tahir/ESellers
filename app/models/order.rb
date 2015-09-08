e22a3ce3107fb82c23c10c77b3cd4691f7809513
Mehreen Tahir <mehreen.tahir@square63.com>
2015-09-08 12:26:59 +0500

    Order: Made userid accessible

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  attr_accessible :session_id, :status, :orignal_price, :discounted_price, :is_discounted
+  attr_accessible :user_id, :status, :orignal_price, :discounted_price, :is_discounted
   validates :orignal_price, numericality: { greater_than_or_equal_to: 0 }
   validates :discounted_price, numericality: { greater_than_or_equal_to: 0 }
 
